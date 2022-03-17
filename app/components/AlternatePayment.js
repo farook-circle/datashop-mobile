6e616c5744f9b2e583004046be91ca4ab3ae086e
SalisYB <salisbell404@gmail.com>
2022-03-17 03:39:09 +0100

    make a big mistake and learn the hardware

---
+++ app/components/AlternatePayment.js
@@ -0,0 +1,85 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import colors from '../../assets/colors/colors';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  getMomoAGentNumber,
+  paymentAccountDetails,
+} from '../redux/actions/wallet';
+
+export default function AlternatePayment() {
+  useEffect(() => {
+    dispatch(paymentAccountDetails());
+  }, []);
+
+  const dispatch = useDispatch();
+  const account_details = useSelector(state => state.wallet.payment_method);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.accountInfoWrapper}>
+        <Text style={styles.bankNameText}>
+          Bank Name:{' '}
+          {account_details !== undefined ? account_details.bank_name : ''}{' '}
+        </Text>
+        <Text style={styles.accountNumberText}>
+          Account Number:{' '}
+          {account_details !== undefined ? account_details.account_number : ''}
+        </Text>
+        <Text style={styles.accountNumberName}>
+          Account Name:{' '}
+          {account_details !== undefined ? account_details.account_name : ''}
+        </Text>
+      </View>
+      <View style={styles.instructionWrapper}>
+        <Text style={styles.instructionText}>
+          Instruction:
+          <Text style={styles.instructionTitleText}>
+            {' '}
+            {account_details !== undefined ? account_details.instruction : ''}
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  accountInfoWrapper: {
+    marginTop: hp('5.9'),
+    width: wp('90%'),
+    borderWidth: 2,
+    padding: hp('2.5'),
+    borderRadius: 10,
+    borderColor: colors.textLight,
+  },
+  bankNameText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: hp('2.08'),
+  },
+  accountNumberText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: hp('2.08'),
+  },
+  accountNumberName: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: hp('2.08'),
+  },
+  instructionWrapper: {
+    marginTop: hp('5.8'),
+    width: wp('90%'),
+  },
+  instructionText: {
+    color: colors.primary,
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: hp('2.5'),
+  },
+});
---
+++ app/components/MomoAgent.js
@@ -0,0 +1,71 @@
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import React, {useEffect} from 'react';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import colors from '../../assets/colors/colors';
+import {useDispatch, useSelector} from 'react-redux';
+import {getMomoAGentNumber} from '../redux/actions/wallet';
+
+export default function ({amount}) {
+  const dispatch = useDispatch();
+  const momo_agent = useSelector(state => state.wallet.momo_agent_number);
+  const ussd = `*502*9*${
+    momo_agent !== undefined ? momo_agent.phone_number : '070xxx'
+  }*${amount}#`;
+
+  useEffect(() => {
+    dispatch(getMomoAGentNumber());
+  }, []);
+
+  const copyToClipboard = () => {
+    Clipboard.setString(ussd);
+  };
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/momo_logo.png')}
+        style={styles.momoAgentLogo}
+      />
+      <Text style={styles.infoText}>
+        Send a FLOAT from your registered phone number to{' '}
+        <Text>{momo_agent !== undefined ? momo_agent.phone_number : ''}</Text>{' '}
+        <Text>{momo_agent !== undefined ? momo_agent.name : ''}</Text>. Your
+        wallet will be credited Automatically.
+      </Text>
+      <TouchableOpacity onPress={copyToClipboard}>
+        <Text style={styles.ussdText}>{ussd}</Text>
+      </TouchableOpacity>
+      <Text style={styles.noteText}>Click the ussd to copy</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  momoAgentLogo: {
+    marginTop: hp('3.90'),
+    width: '80%',
+    height: hp('26.0'),
+  },
+  infoText: {
+    marginTop: hp('2.5'),
+    fontFamily: 'Poppins-Regular',
+    fontSize: hp('2.5'),
+    paddingHorizontal: 25,
+  },
+  ussdText: {
+    marginTop: hp('5%'),
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: hp('2.5'),
+    color: colors.primary,
+  },
+  noteText: {
+    fontFamily: 'Poppins-Regular',
+  },
+});

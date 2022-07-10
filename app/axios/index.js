5e8bd43284cafb9c555f06146ee3e81844138275
SalisYB <salisbell404@gmail.com>
2022-07-10 10:47:14 +0100

    add headers

--- app/axios/index.js
+++ app/axios/index.js
@@ -3,15 +3,19 @@ import axios from 'axios';
 // for development server
 // export default axios.create({
 //   baseURL: 'http://192.168.43.39:8000',
+// headers: {
 //   'X-Platform': 'android',
-//   'X-App-Build-Number': '1.0.01',
-//   'X-Col-Build-Number': '1234567890',
+//   'X-App-Build-Number': '1.2.0',
+//   'X-Col-Build_Number': '0000000000',
+// },
 // });
 
 // for production server
 export default axios.create({
   baseURL: 'https://data-shop-server-application.herokuapp.com',
-  'X-Platform': 'android',
-  'X-App-Build-Number': '1.0.01',
-  'X-Col-Build_Number': '0000000000',
+  headers: {
+    'X-Platform': 'android',
+    'X-App-Build-Number': '1.2.0',
+    'X-Col-Build_Number': '0000000000',
+  },
 });
